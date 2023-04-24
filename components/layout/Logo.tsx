720ada48a893f0598cc5f52dfd0a845fd8911dd2
Harsh <harshgupta3341@gmail.com>
2023-04-24 21:51:33 +0530

    partially done with landing page

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = ({ width, height }: any) => {
   return (
     <div className="flex justify-center items-center h-full">
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width={width}
-        height={height}
-        placeholder="blur"
-        blurDataURL="/logo.png"
-      />
+      <Link href="/home">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={width}
+          height={height}
+          placeholder="blur"
+          blurDataURL="/logo.png"
+        />
+      </Link>
     </div>
   );
 };
