595f2dc3215fd314886e2b5f96b345f2b0ff9914
Harsh <harshgupta3341@gmail.com>
2023-07-30 20:49:56 +0530

    Revert "added redis in videos route"
    This reverts commit 0edf1d41237adf8d1065a30006572b75c61a5bdb.

--- pages/api/videos/index.ts
+++ pages/api/videos/index.ts
@@ -1,21 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/libs/prismadb";
 
-import { redis } from "@/libs/redis";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
     const { q } = req.query;
-
-    const cachedData = await redis.get(`videos:${q}`);
-
-    if (cachedData) {
-      return res.status(200).json(JSON.parse(cachedData));
-    }
-
     const data = await prisma.video.findMany({
       where: {
         type: "youtube#video",
@@ -24,9 +15,6 @@ export default async function handler(
         } as any,
       },
     });
-
-    await redis.setex(`videos:${q}`, 60 * 60 * 24, JSON.stringify(data));
-
     res.status(200).json(data);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
